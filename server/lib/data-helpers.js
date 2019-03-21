29587f98cef7cfa1ee7c7181e53362bffbbff0c8
boksul <boksul@hotmail.com>
2019-03-21 19:00:54 +0000

    MongoDB added

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -10,18 +10,17 @@ module.exports = function makeDataHelpers(db) {
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
       simulateDelay(() => {
-        db.tweets.push(newTweet);
+        db.collection('tweeter').insertOne(newTweet);
         callback(null, true);
       });
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
+      const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+      db.collection('tweeter').find().toArray(function(err, result) {
+        callback(null, result.sort(sortNewestFirst))
+      })
     }
-
   };
 }
